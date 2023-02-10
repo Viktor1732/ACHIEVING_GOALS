dded71be128106ac4db99433e3cc3101fe102110
Viktor1732 <programmer.kras@gmail.com>
2023-02-10 22:12:19 +0700

    Добавлены функции, пути и шаблоны меню. (с заглушками)

--- goals/goal_setting/urls.py
+++ goals/goal_setting/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from goal_setting.views import *
 
 urlpatterns = [
-    path('', index, name='home')
+    path('', index, name='home'),
+    path('goals_menu/', show_goals_menu, name='goals'),
+    path('info/', show_info, name='info'),
+    path('contact/', contact, name='contact'),
 ]
