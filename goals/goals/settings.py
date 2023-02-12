63f2347973f8a111d42352c0ae511b15eed5e9c0
Viktor1732 <programmer.kras@gmail.com>
2023-02-12 10:24:40 +0700

    Удалил все и решил начать проект заново!!!

--- goals/goals/settings.py
+++ goals/goals/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'goal_setting.apps.GoalSettingConfig',
+    'goals_setting.apps.GoalSettingConfig',
 ]
 
 MIDDLEWARE = [
