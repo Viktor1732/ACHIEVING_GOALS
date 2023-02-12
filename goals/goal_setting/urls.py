7208958fa7f1534604abe7840052092090803a30
Viktor1732 <programmer.kras@gmail.com>
2023-02-12 22:25:41 +0700

    Прописал функции представления, urls и шаблоны с заглушками.

--- goals/goal_setting/urls.py
+++ goals/goal_setting/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
-from goal_setting.views import index
+from goal_setting.views import *
 
 urlpatterns = [
-    path('', index, name='home')
+    path('', index, name='home'),
+    path('why_us/', why_us, name='why_us'),
+    path('goals_how_it_work/', goals_info, name='goals_info'),
+    path('points_how_it_work/', points_info, name='points_info'),
+    path('goals/', show_all_goals, name='all_goals'),
+    path('news/', show_news, name='news'),
+    path('about/', about_us, name='about'),
+    path('leaders/', show_leaders, name='leaders'),
+    path('privacy_policy/', privacy_policy, name='privacy_policy'),
+    path('condition_of_use/', condition_of_use, name='condition_of_use'),
+    path('contact/', contact, name='contact'),
 ]
--- goals/goal_setting/views.py
+++ goals/goal_setting/views.py
@@ -3,3 +3,43 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'goal_setting/index.html', context={'title': 'Лучшее приложение для достижения целей!'})
+
+
+def why_us(request):
+    return render(request, 'goal_setting/why_us.html', context={'title': 'Почему Мы?'})
+
+
+def goals_info(request):
+    return render(request, 'goal_setting/goals_info.html', context={'title': 'Информация о целях'})
+
+
+def points_info(request):
+    return render(request, 'goal_setting/points_info.html', context={'title': 'Информация о баллах'})
+
+
+def show_all_goals(request):
+    return render(request, 'goal_setting/goals.html', context={'title': 'Последние опубликованные цели'})
+
+
+def show_news(request):
+    return render(request, 'goal_setting/news.html', context={'title': 'Новости'})
+
+
+def about_us(request):
+    return render(request, 'goal_setting/about.html', context={'title': 'О нас'})
+
+
+def show_leaders(request):
+    return render(request, 'goal_setting/leader_board.html', context={'title': 'Наши лидеры'})
+
+
+def privacy_policy(request):
+    return render(request, 'goal_setting/privacy_policy.html', context={'title': 'Политика конфиденциальности'})
+
+
+def condition_of_use(request):
+    return render(request, 'goal_setting/condition_of_use.html', context={'title': 'Условия пользования'})
+
+
+def contact(request):
+    return render(request, 'goal_setting/contact.html', context={'title': 'Обратная связь'})
