9277b6acb445c6a907d38f8bcafb7d5cccffa1e0
Viktor1732 <programmer.kras@gmail.com>
2023-02-12 23:18:41 +0700

    1. Добавил эффект подчеркивания в меню; 2. Изменил расположение "Таблицы лидеров" в меню.

--- goals/goal_setting/views.py
+++ goals/goal_setting/views.py
@@ -21,16 +21,16 @@ def show_all_goals(request):
     return render(request, 'goal_setting/goals.html', context={'title': 'Последние опубликованные цели'})
 
 
+def show_leaders(request):
+    return render(request, 'goal_setting/leader_board.html', context={'title': 'Наши лидеры'})
+
+
 def show_news(request):
     return render(request, 'goal_setting/news.html', context={'title': 'Новости'})
 
 
 def about_us(request):
     return render(request, 'goal_setting/about.html', context={'title': 'О нас'})
-
-
-def show_leaders(request):
-    return render(request, 'goal_setting/leader_board.html', context={'title': 'Наши лидеры'})
 
 
 def privacy_policy(request):
