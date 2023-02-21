083ca05ede80a86d98398550c87f466e1c1c2435
Viktor1732 <programmer.kras@gmail.com>
2023-02-21 19:35:14 +0700

    Создал модель News для отображения опубликованных  новостей на странице.

--- goals/goal_setting/models.py
+++ goals/goal_setting/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Заголовок')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    image = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Изображение', null=True)
+    is_published = models.BooleanField(default=True, verbose_name='Публикация')
+
+    def __str__(self):
+        return self.title
