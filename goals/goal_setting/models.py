45254086c4af7f5c5c2cc027a6a134feb1a625c5
Viktor1732 <programmer.kras@gmail.com>
2023-02-23 22:55:42 +0700

    Прописал модель создания цели.

--- goals/goal_setting/models.py
+++ goals/goal_setting/models.py
@@ -2,6 +2,23 @@ from django.db import models
 from django.urls import reverse
 
 
+class Goals(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название цели')
+    description = models.TextField(blank=True, verbose_name='Описание цели')
+    category = models.CharField(verbose_name='Категория')
+    time_of_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    time_of_end = models.DateTimeField(verbose_name='Дата завершения')
+    image = models.ImageField(upload_to='photos/goals/%Y/%m/%d/', verbose_name='Изображение', null=True)
+    is_published = models.BooleanField(default=False, verbose_name='Публикация')
+    is_completed = models.BooleanField(verbose_name='Завершенность')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('goal', kwargs={'goal': self.title})
+
+
 class News(models.Model):
     title = models.CharField(max_length=150, verbose_name='Заголовок')
     slug = models.SlugField(max_length=50, unique=True, db_index=True, verbose_name="URL")
